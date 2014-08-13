6eebd7e467e88b42ac49bd4ab5fa1c792e7d3bfb
Assaf Gelber <assaf.gelber@gmail.com>
2014-08-13 17:18:50 +0300

    Remove jquery from make_zip

--- make_zip.sh
+++ make_zip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-zip github-cleandiff.zip github-cleandiff.js github-cleandiff.css manifest.json jquery-1.11.1.min.js
+zip github-cleandiff.zip github-cleandiff.js github-cleandiff.css manifest.json

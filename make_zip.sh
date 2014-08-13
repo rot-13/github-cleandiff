8e7851b16ac0320ae4b2db081cb375c95afcd6f3
Gilad Goldberg <giladgo@gmail.com>
2014-08-13 17:53:01 +0300

    adding screenshots and icons

--- make_zip.sh
+++ make_zip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-zip github-cleandiff.zip github-cleandiff.js github-cleandiff.css manifest.json
+rm -f github-cleandiff.zip
+zip github-cleandiff.zip github-cleandiff.js github-cleandiff.css manifest.json logo128.png logo48.png logo16.png

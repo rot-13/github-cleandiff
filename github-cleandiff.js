6e3b373535e9297f02c938654d9237a6c93e174f
Assaf Gelber <assaf.gelber@gmail.com>
2014-08-13 17:19:37 +0300

    Add vars because this isn't coffeescript :unamused:

--- github-cleandiff.js
+++ github-cleandiff.js
@@ -1,16 +1,16 @@
-lines = document.querySelectorAll('.diff-line-code');
+var lines = document.querySelectorAll('.diff-line-code');
 
 for (var i = 0; i < lines.length; i++) {
-  line = lines[i];
-  lineContent = line.innerText;
-  firstChar = lineContent[0];
-  restContent = lineContent.slice(1);
+  var line = lines[i],
+      lineContent = line.innerText,
+      firstChar = lineContent[0],
+      restContent = lineContent.slice(1);
   
-  first = document.createElement('span');
+  var first = document.createElement('span');
   first.classList.add('clean-diff-sheker');
   first.setAttribute('data-char', firstChar);
 
-  rest = document.createElement('span');
+  var rest = document.createElement('span');
   rest.innerText = restContent;
 
   line.innerText = '';

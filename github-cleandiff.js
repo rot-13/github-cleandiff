e24f785c12d3375b43d91df5e8f09871b7b8d88b
Assaf Gelber <assaf.gelber@gmail.com>
2014-08-13 17:15:45 +0300

    Remove jquery dependency

--- github-cleandiff.js
+++ github-cleandiff.js
@@ -1,9 +1,19 @@
-$lines = $('.diff-line-code');
+lines = document.querySelectorAll('.diff-line-code');
 
-for (var i = 0; i < $lines.length; i++) {
-  $line = $($lines[i]);
-  lineContent = $line.text();
+for (var i = 0; i < lines.length; i++) {
+  line = lines[i];
+  lineContent = line.innerText;
   firstChar = lineContent[0];
-  $line.text(lineContent.slice(1));
-  $('<span class="clean-diff-sheker">').attr('data-char', firstChar).prependTo($line);
+  restContent = lineContent.slice(1);
+  
+  first = document.createElement('span');
+  first.classList.add('clean-diff-sheker');
+  first.setAttribute('data-char', firstChar);
+
+  rest = document.createElement('span');
+  rest.innerText = restContent;
+
+  line.innerText = '';
+  line.appendChild(first);
+  line.appendChild(rest);
 }

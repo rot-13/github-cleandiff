46896848de9eae21c4b239ae951acacc5667a448
Gilad Goldberg <giladgo@gmail.com>
2014-08-20 17:31:45 +0300

    fixing onload

--- github-cleandiff.js
+++ github-cleandiff.js
@@ -1,21 +1,23 @@
-var lines = document.querySelectorAll('.diff-line-code');
+window.addEventListener("load", function() {
+  var lines = document.querySelectorAll('.diff-line-code');
 
-for (var i = 0; i < lines.length; i++) {
-  var line = lines[i],
-      commentButton = line.firstChild,
-      lineContent = line.innerText,
-      firstChar = lineContent[0],
-      restContent = lineContent.slice(1);
-  
-  var first = document.createElement('span');
-  first.classList.add('clean-diff-sheker');
-  first.setAttribute('data-char', firstChar);
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i],
+        commentButton = line.firstChild,
+        lineContent = line.innerText,
+        firstChar = lineContent[0],
+        restContent = lineContent.slice(1);
 
-  var rest = document.createElement('span');
-  rest.innerText = restContent;
+    var first = document.createElement('span');
+    first.classList.add('clean-diff-sheker');
+    first.setAttribute('data-char', firstChar);
 
-  line.innerText = '';
-  line.appendChild(commentButton);
-  line.appendChild(first);
-  line.appendChild(rest);
-}
+    var rest = document.createElement('span');
+    rest.innerText = restContent;
+
+    line.innerText = '';
+    line.appendChild(commentButton);
+    line.appendChild(first);
+    line.appendChild(rest);
+  }
+}, false);
